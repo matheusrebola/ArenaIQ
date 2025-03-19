adb12a6ec3183122608ebeb9e932c456852b6a2c
Matheus Rebola <m.cavalcantir@outlook.com>
2025-03-19 16:04:40 -0300

    feat: correção do tipo de dado passado como parâmetro na repository

--- services/dados-historicos/src/main/java/arenaiq/dadoshistoricos/core/repositories/JogadoresElencoRepository.java
+++ services/dados-historicos/src/main/java/arenaiq/dadoshistoricos/core/repositories/JogadoresElencoRepository.java
@@ -3,9 +3,9 @@ package arenaiq.dadoshistoricos.core.repositories;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
-import arenaiq.dadoshistoricos.core.models.Jogadores;
+import arenaiq.dadoshistoricos.core.models.JogadoresElenco;
 
 @Repository
-public interface JogadoresElencoRepository extends CassandraRepository<Jogadores, String>{
+public interface JogadoresElencoRepository extends CassandraRepository<JogadoresElenco, String>{
 
 }
