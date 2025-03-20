361cfb0bb25b27887d856fa5b7d3b3eef66df21d
Matheus Rebola <m.cavalcantir@outlook.com>
2025-03-20 15:40:53 -0300

    feat: criação do script para settar usuário e senha para os bancos de dados

---
+++ services/mongo-init.js
@@ -0,0 +1,22 @@
+// Criar os bancos de dados manualmente e adicionar usuários
+const databases = [
+  { name: "partida-db", user: "partida", password: "partida" },
+  { name: "treino-db", user: "treino", password: "treino" },
+  { name: "encaminhador-db", user: "encaminhador", password: "encaminhador" },
+  { name: "recebidor-db", user: "recebidor", password: "recebidor" }
+];
+
+databases.forEach(({ name, user, password }) => {
+  db = db.getSiblingDB(name);
+  db.createCollection("initCollection"); // Garante a criação do banco
+  print(`Banco ${name} criado com sucesso.`);
+
+  db.createUser({
+    user: user,
+    pwd: password,
+    roles: [{ role: "readWrite", db: name }]
+  });
+  print(`Usuário ${user} criado com sucesso no banco ${name}.`);
+});
+
+print("Todos os bancos e usuários foram criados com sucesso!");
