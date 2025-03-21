947942092f4e7d89cac16f43ac98877f9de7118b
Matheus Rebola <m.cavalcantir@outlook.com>
2025-03-21 11:20:22 -0300

    feat: configuração do método para criação de partidas históricas

--- services/partida/src/main/java/arenaiq/partida/core/controllers/PartidaController.java
+++ services/partida/src/main/java/arenaiq/partida/core/controllers/PartidaController.java
@@ -1,7 +1,5 @@
 package arenaiq.partida.core.controllers;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +7,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import arenaiq.partida.core.dtos.JogadorDTO;
 import arenaiq.partida.core.dtos.PartidaCreateDTO;
 import arenaiq.partida.core.dtos.PartidaDTO;
 import arenaiq.partida.core.mappers.PartidaMapper;
 import arenaiq.partida.core.models.Partida;
-import arenaiq.partida.core.repositories.JogadoresElencoRepository;
 import arenaiq.partida.core.services.JogadorService;
 import arenaiq.partida.core.services.PartidaService;
 import lombok.RequiredArgsConstructor;
@@ -26,11 +22,6 @@ public class PartidaController {
   private final PartidaMapper pMapper;
   private final PartidaService pService;
   private final JogadorService jService;
-  private final JogadoresElencoRepository repository;
-
-  public List<JogadorDTO> encontrarJogadores(String elenco, String temporada){
-    return repository.findJogadorByElencoAndTemporada(elenco, temporada);
-  }
 
   @PostMapping("/historica")
   public ResponseEntity<PartidaDTO> criar(@RequestBody PartidaCreateDTO request){
