d316b4cd01feb6698307ca9cb14c0fb3e3a5d9f7
Matheus Rebola <m.cavalcantir@outlook.com>
2025-04-07 19:44:43 -0300

    feat: melhoria nos atributos da classe

--- services/recebidor/movimentacao/src/main/java/arenaiq/recebidor/movimentacao/core/models/Movimentacoes.java
+++ services/recebidor/movimentacao/src/main/java/arenaiq/recebidor/movimentacao/core/models/Movimentacoes.java
@@ -12,11 +12,10 @@ public class Movimentacoes {
   private String id;
   private Float eixoX;
   private Float eixoY;
-  private Float velocidade;
-  private Float aceleracao;
+  private Float velocidadeX;
+  private Float velocidadeY;
   private Byte minuto;
   private Byte segundo;
-  private Boolean pressao;
   private String partidaId;
   private String jogadorId;
 }
