798ae7b4e8b2d0a47089e3c68df3cb62efa3fd9b
Matheus Rebola <m.cavalcantir@outlook.com>
2025-04-07 10:26:54 -0300

    feat: configuração final das models e seus enums

--- services/treino/src/main/java/arenaiq/treino/core/models/Linhas.java
+++ services/treino/src/main/java/arenaiq/treino/core/models/Linhas.java
@@ -3,7 +3,9 @@ package arenaiq.treino.core.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import arenaiq.treino.core.models.enums.ELargura;
 import arenaiq.treino.core.models.enums.ELinha;
+import arenaiq.treino.core.models.enums.EModoJogo;
 import lombok.Data;
 
 @Document
@@ -12,7 +14,7 @@ public class Linhas {
   @Id
   private String id;
   private ELinha linha;
-  private Float altura;
-  private Float largura;
+  private EModoJogo altura;
+  private ELargura largura;
   private String url;
 }
---
+++ services/treino/src/main/java/arenaiq/treino/core/models/enums/ELargura.java
@@ -0,0 +1,5 @@
+package arenaiq.treino.core.models.enums;
+
+public enum ELargura {
+  ULTRA_FECHADA, FECHADO, NEUTRO, ABERTO, ULTRA_ABERTO
+}
