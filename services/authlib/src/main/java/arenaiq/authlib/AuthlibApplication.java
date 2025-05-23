62609c58fbd235b9f603ba26b437a05a648cd329
Matheus Rebola <m.cavalcantir@outlook.com>
2025-05-23 14:42:56 -0300

    feat: criação da biblioteca para auxiliar na autenticação stateless entre os microserviços da saga

---
+++ services/authlib/src/main/java/arenaiq/authlib/AuthlibApplication.java
@@ -0,0 +1,13 @@
+package arenaiq.authlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthlibApplication.class, args);
+	}
+
+}
---
+++ services/authlib/src/test/java/arenaiq/authlib/AuthlibApplicationTests.java
@@ -0,0 +1,13 @@
+package arenaiq.authlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
