fecb8cdcdaf923527f22bdcbb0e9e2d5497efb02
Matheus Rebola <m.cavalcantir@outlook.com>
2025-07-02 19:13:54 -0300

    feat: atualização do projeto

--- services/encaminhador/evento/src/main/java/arenaiq/encaminhador/evento/config/exception/ExceptionGlobalHandler.java
+++ services/encaminhador/evento/src/main/java/arenaiq/encaminhador/evento/config/exception/ExceptionGlobalHandler.java
@@ -13,4 +13,11 @@ public class ExceptionGlobalHandler {
         var details = new ExceptionDetails(HttpStatus.BAD_REQUEST.value(), validationException.getMessage());
         return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<?> handleAuthenticationException(AuthenticationException authenticationException) {
+        var details = new ExceptionDetails(HttpStatus.UNAUTHORIZED.value(), authenticationException.getMessage());
+        return new ResponseEntity<>(details, HttpStatus.UNAUTHORIZED);
+    }
+
 }
